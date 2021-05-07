5e5af788dcc0653c1ea22df2bd207225b814551f
raghurajj <raghurajpratap1999@gmail.com>
2021-05-07 17:45:14 +0530

    added deployment setting

--- rest/settings.py
+++ rest/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from datetime import timedelta
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -168,5 +169,7 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+django_heroku.settings(locals())
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
